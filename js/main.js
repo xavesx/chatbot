fe98060306f37a6b96a06083f85848547f28de8b
Aves <aavesbilal@gmail.com>
2024-11-26 09:19:41 +0530

    Improved user interactivity

--- js/main.js
+++ js/main.js
@@ -8,17 +8,28 @@ var user = {message:""};
 var arrayOfPossibleMessage = [
     // {"message":"", "response":""}
     {"message":"how are you?", "response":"I'm great"},
-    {"message":"hi.", "response":"Hello!"},
+    {"message":"hi.", "response":"Hey there!"},
+    {"message":"hello.", "response":"Hello!"},
     {"message":"who are you?", "response":"I'm your assistant"},
     {"message":"what's your name?", "response":"My name is Rek"},
     {"message":"what is your name?", "response":"My name is Rek"},
     {"message":"how old are you?", "response":"I'm ageless "},
+    {"message":"what can you do?", "response":"I can chat with you."},
+    {"message":"tell me a joke.", "response":"Why don't scientists trust atoms? Because they make up everything!"},
+    {"message":"what is the weather like?", "response":"I can't check the weather right now, but I hope it's nice!"},
+    {"message":"what is your favorite color?", "response":"I like all the colors equally."},
+    {"message":"do you like music?", "response":"Yes, I love music!"},
+    {"message":"what is your favorite food?", "response":"I don't eat, but I hear pizza is great."},
+    {"message":"where are you from?", "response":"I'm from the digital world."},
+    {"message":"can you help me?", "response":"Of course! What do you need help with?"},
+    {"message":"what is your purpose?", "response":"My purpose is to assist you."},
+    {"message":"do you have any hobbies?", "response":"I enjoy chatting with you."}
 
 ]
 
-setTimeout(function(){
-    chatbotSendMessage("Hi, I'm Rek What is your name?");
-},1000);
+// setTimeout(function(){
+//     chatbotSendMessage("Hi, I'm Rek What is your name?");
+// },1000);
 
 
 
